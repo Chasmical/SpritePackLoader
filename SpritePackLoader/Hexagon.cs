456461c71d7bc96d0da6279832ca72414982132e
Abbysssal <55982389+Abbysssal@users.noreply.github.com>
2021-06-07 20:48:12 +0700

    Add project files.

---
+++ SpritePackLoader/Hexagon.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace SpritePackLoader.Hexagons
+{
+	public struct Hexagon
+	{
+		private const float Sqrt3 = 1.732050808f;
+		private const float Sqrt3Over2 = 0.866025403f;
+		private static readonly Hexagon[] directions = new Hexagon[6]
+		{
+			new Hexagon(1, -1, 0), new Hexagon(1, 0, -1), new Hexagon(0, 1, -1),
+			new Hexagon(-1, 1, 0), new Hexagon(-1, 0, 1), new Hexagon(0, -1, 1)
+		};
+		public Hexagon this[int direction] => this + directions[direction];
+
+		public Hexagon(int x, int y, int z) { X = x; Y = y; Z = z; }
+		public int X, Y, Z;
+		public Vector3 ToVector(float size) => size * new Vector3(1.5f * X, Sqrt3Over2 * X + Sqrt3 * Z);
+
+		public static IEnumerable<Hexagon> Ring(Hexagon center, int radius)
+		{
+			Hexagon coord = center + directions[4] * radius;
+			for (int i = 0; i < 6; i++)
+				for (int j = 0; j < radius; j++)
+				{
+					yield return coord;
+					coord = coord[i];
+				}
+		}
+		public static IEnumerable<Hexagon> Spiral(Hexagon center, int radius)
+		{
+			for (int k = 1; k <= radius; k++)
+				foreach (Hexagon hexagon in Ring(center, k))
+					yield return hexagon;
+		}
+
+		public static Hexagon operator +(Hexagon a, Hexagon b) => new Hexagon(a.X + b.X, a.Y + b.Y, a.Z + b.Z);
+		public static Hexagon operator *(Hexagon hexagon, int scale) => new Hexagon(hexagon.X * scale, hexagon.Y * scale, hexagon.Z * scale);
+
+	}
+}
---
+++ SpritePackLoader/SpritePackTester.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RogueLibsCore;
+using SpritePackLoader.Hexagons;
+
+namespace SpritePackLoader
+{
+	[ItemCategories(RogueCategories.Technology, RogueCategories.Usable, RogueCategories.NPCsCantPickUp)]
+	public class SpritePackTester : CustomItem, IItemUsable
+	{
+		public override void SetupDetails()
+		{
+			Item.itemType = ItemTypes.Tool;
+			Item.itemValue = 488755541;
+			Item.initCount = 1;
+			Item.rewardCount = 1;
+			Item.hasCharges = true;
+			Item.goesInToolbar = true;
+			Item.LoadItemSprite("PropertyDeed");
+		}
+		private static bool displayed;
+		private static bool activating;
+		private static readonly List<InvItem> decoys = new List<InvItem>();
+		private IEnumerable<Vector3> EnumerateLocationsNearby(float scale)
+			=> Hexagon.Spiral(new Hexagon(0, 0, 0), int.MaxValue).Select(h => h.ToVector(scale));
+		public void UseItem()
+		{
+			if (activating) return;
+			activating = true;
+			Item.database.StartCoroutine(UseItem2());
+		}
+		private IEnumerator UseItem2()
+		{
+			try
+			{
+				if (displayed = !displayed)
+				{
+					Vector3 center = Owner.tr.position;
+					IEnumerator<Vector3> locationsEnumerator = EnumerateLocationsNearby(0.32f).GetEnumerator();
+
+					foreach (ItemUnlock item in RogueLibsInternals.Unlocks.OfType<ItemUnlock>())
+					{
+						InvItem invItem = new InvItem { invItemName = item.Name };
+						invItem.SetupDetails(false);
+						invItem.invItemCount = 0;
+						invItem.Categories.Add("Decoy");
+
+						if (!locationsEnumerator.MoveNext()) break;
+						Vector3 location = locationsEnumerator.Current + center;
+						Item spawned = GameController.gameController.spawnerMain.SpawnItem(location, invItem);
+						GameController.gameController.audioHandler.Play(spawned, "Spawn");
+						decoys.Add(invItem);
+						yield return new WaitForFixedUpdate();
+					}
+				}
+				else
+				{
+					foreach (Item item in GameController.gameController.itemList.ToList())
+						if (item.invItem?.Categories?.Contains("Decoy") == true)
+						{
+							GameController.gameController.audioHandler.Play(item, "Spawn");
+							item.DestroyMe();
+							yield return new WaitForFixedUpdate();
+						}
+
+					foreach (Agent obj in GameController.gameController.agentList)
+					{
+						InvDatabase db = obj.agentInvDatabase ?? obj.objectInvDatabase ?? obj.specialInvDatabase ?? obj.playerInvDatabase;
+						bool playSound = false;
+						if (db != null)
+							foreach (InvItem item in db.InvItemList)
+								if (item.Categories.Contains("Decoy"))
+								{
+									db.DestroyItem(item);
+									playSound = true;
+								}
+						if (playSound) GameController.gameController.audioHandler.Play(db.objectReal, "Spawn");
+					}
+
+					decoys.Clear();
+				}
+			}
+			finally
+			{
+				activating = false;
+			}
+		}
+	}
+}
